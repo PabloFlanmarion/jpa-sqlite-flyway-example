1bc61e826a575b56d26d7879c274e7a33f41848b
PabloFLanmarion <pabloflanmarion@gmail.com>
2020-07-29 19:50:45 -0300

    Second Commit

---
+++ src/main/java/com/example/JpaSqliteFlywayExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSqliteFlywayExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSqliteFlywayExampleApplication.class, args);
+	}
+
+}
